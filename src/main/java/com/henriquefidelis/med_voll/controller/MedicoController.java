a277075b989171a4ffb642e60f01257c65b284eb
Henrique Fidelis <fidelis.henrique.jh@gmail.com>
2025-08-06 17:50:39 -0300

    Configurando classes para tabela de médicos

--- src/main/java/com/henriquefidelis/med_voll/controller/MedicoController.java
+++ src/main/java/com/henriquefidelis/med_voll/controller/MedicoController.java
@@ -1,6 +1,10 @@
 package com.henriquefidelis.med_voll.controller;
 
 import com.henriquefidelis.med_voll.medico.DadosCadastroMedico;
+import com.henriquefidelis.med_voll.medico.Medico;
+import com.henriquefidelis.med_voll.medico.MedicoRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,10 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/medicos")
 public class MedicoController {
+    
+    @Autowired
+    private MedicoRepository repository;
 
     @PostMapping
     public void cadastrar(@RequestBody DadosCadastroMedico dados) {
-        System.out.println(dados);
+        repository.save(new Medico(dados));
     }
 
 }
