d9e0cb7cad57d38c5d5145a2122d9b70c6b8f902
jhfidelis <fidelis.henrique.jh@gmail.com>
2025-08-07 11:34:01 -0300

    Listando médicos

--- src/main/java/com/henriquefidelis/med_voll/controller/MedicoController.java
+++ src/main/java/com/henriquefidelis/med_voll/controller/MedicoController.java
@@ -1,16 +1,16 @@
 package com.henriquefidelis.med_voll.controller;
 
 import com.henriquefidelis.med_voll.medico.DadosCadastroMedico;
+import com.henriquefidelis.med_voll.medico.DadosListagemMedico;
 import com.henriquefidelis.med_voll.medico.Medico;
 import com.henriquefidelis.med_voll.medico.MedicoRepository;
 
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -23,6 +23,14 @@ public class MedicoController {
     @Transactional
     public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
         repository.save(new Medico(dados));
+    }
+
+    @GetMapping
+    public List<DadosListagemMedico> listarMedicos() {
+        return repository.findAll()
+                .stream()
+                .map(DadosListagemMedico::new)
+                .toList();
     }
 
 }
---
+++ src/main/java/com/henriquefidelis/med_voll/medico/DadosListagemMedico.java
@@ -0,0 +1,14 @@
+package com.henriquefidelis.med_voll.medico;
+
+public record DadosListagemMedico(
+        String nome,
+        String email,
+        String crm,
+        Especialidade especialidade
+) {
+
+    public DadosListagemMedico(Medico medico) {
+        this(medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
+    }
+
+}
