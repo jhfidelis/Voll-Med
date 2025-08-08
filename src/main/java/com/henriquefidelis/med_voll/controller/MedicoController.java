6afbb1d7d996ccdfbf2a8fad4269dd9b865ebb9f
jhfidelis <fidelis.henrique.jh@gmail.com>
2025-08-08 09:27:34 -0300

    Atualizando dados do médico

--- src/main/java/com/henriquefidelis/med_voll/controller/MedicoController.java
+++ src/main/java/com/henriquefidelis/med_voll/controller/MedicoController.java
@@ -1,9 +1,6 @@
 package com.henriquefidelis.med_voll.controller;
 
-import com.henriquefidelis.med_voll.medico.DadosCadastroMedico;
-import com.henriquefidelis.med_voll.medico.DadosListagemMedico;
-import com.henriquefidelis.med_voll.medico.Medico;
-import com.henriquefidelis.med_voll.medico.MedicoRepository;
+import com.henriquefidelis.med_voll.medico.*;
 
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,8 +9,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -32,6 +27,13 @@ public class MedicoController {
     public Page<DadosListagemMedico> listarMedicos(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
         return repository.findAll(paginacao)
                 .map(DadosListagemMedico::new);
+    }
+
+    @PutMapping
+    @Transactional
+    public void atualizarMedico(@RequestBody @Valid DadosAtualizacaoMedico dados) {
+        var medico = repository.getReferenceById(dados.id());
+        medico.atualizarInformacoes(dados);
     }
 
 }
---
+++ src/main/java/com/henriquefidelis/med_voll/medico/DadosAtualizacaoMedico.java
@@ -0,0 +1,14 @@
+package com.henriquefidelis.med_voll.medico;
+
+import com.henriquefidelis.med_voll.endereco.DadosEndereco;
+import jakarta.validation.constraints.NotNull;
+
+public record DadosAtualizacaoMedico(
+        @NotNull
+        Long id,
+
+        String nome,
+        String telefone,
+        DadosEndereco endereco
+) {
+}
