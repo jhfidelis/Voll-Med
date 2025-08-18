f144f1c3f93bc5ac744bd07c1f11e4e26466973f
jhfidelis <fidelis.henrique.jh@gmail.com>
2025-08-18 10:23:24 -0300

    Configurando o banco de testes

---
+++ src/test/java/com/henriquefidelis/med_voll/domain/medico/MedicoRepositoryTest.java
@@ -0,0 +1,17 @@
+package com.henriquefidelis.med_voll.domain.medico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
+public class MedicoRepositoryTest {
+
+    @Test
+    void testEscolherMedicoAleatoriamente() {
+
+    }
+}
